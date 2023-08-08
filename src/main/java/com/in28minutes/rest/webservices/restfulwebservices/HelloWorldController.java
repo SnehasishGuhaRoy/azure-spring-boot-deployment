10b30613cacd64398e71e5d9bc8ad0d0fefe688c
SNEHASISH GUHA ROY <snehasishguharoy@SNEHASISHs-MacBook-Pro.local>
2023-08-09 01:33:21 +0530

    changed to v2

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -9,19 +9,19 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World to the M2 Series Processor";
+		return "Hello World to the M2 Series Processor v2";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		// throw new RuntimeException("Some Error has Happened! Contact Support at
 		// ***-***");
-		return new HelloWorldBean("Hello World to the M2 Series Processor");
+		return new HelloWorldBean("Hello World to the M2 Series Processor v2");
 	}
 
 	/// hello-world/path-variable/in28minutes
 	@GetMapping(path = "/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
-		return new HelloWorldBean(String.format("Hello World, %s", name));
+		return new HelloWorldBean(String.format("Hello World v2, %s", name));
 	}
 }
